779b1ccfc8ab0741448d66c575dd92bc3fb99daf
narendra-cyber <76886842+narendra-cyber@users.noreply.github.com>
2021-06-07 15:40:20 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,25 +20,25 @@ function setup() {
    }
 
 
-    for (var j = 75; j <=width; j=j+50) 
+    for (var j = 75; j<=width; j=j+50) 
     {
     
        plinkos.push(new Plinko(j,75));
     }
 
-    for (var j = 50; j <=width-10; j=j+50) 
+    for (var j = 50; j<=width-10; j=j+50) 
     {
     
        plinkos.push(new Plinko(j,175));
     }
 
-     for (var j = 75; j <=width; j=j+50) 
+     for (var j=75; j <=width; j=j+50) 
     {
     
        plinkos.push(new Plinko(j,275));
     }
 
-     for (var j = 50; j <=width-10; j=j+50) 
+     for (var j=50; j <=width-10; j=j+50) 
     {
     
        plinkos.push(new Plinko(j,375));
